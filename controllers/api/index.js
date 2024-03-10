7fe243bc7bb014e844caa60e589babce090b2137
Bernardo Alonso <bernardo4430@gmail.com>
2024-03-10 00:50:20 -0600

    fixing some bugs

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const shoppingCartRoutes = require('./shoppingCartRoutes');
 const recipeRoutes = require('./recipeRoutes');
 const userRoutes = require('./userRoutes');
 const mealRoutes = require('./mealRoutes');
-const recipeRoutes = require('./recipeRoutes');
+
 
 router.use('/recipe-routes', recipeRoutes);
 router.use('/shopping-cart', shoppingCartRoutes);
