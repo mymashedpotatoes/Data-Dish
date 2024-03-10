320f60f69e997994210ace0f9d3fbcc3c29476d9
ella gaul <145066673+mymashedpotatoes@users.noreply.github.com>
2024-03-10 00:47:38 -0600

    fixed put recipe route bug :P

--- controllers/api/shoppingCartRoutes.js
+++ controllers/api/shoppingCartRoutes.js
@@ -1,14 +1,11 @@
 // Route to add an item to the shopping cart 
 const { ShoppingCartItem, ShoppingCart } = require("../../models");
 const express = require("express");
+const withAuth = require("../../utils/auth");
 const router = express.Router();
 
-router.post("/add-item", async (req, res) => {
-<<<<<<< HEAD
-    const { name, amount } = req.body;
-=======
+router.post("/add-item", withAuth,async (req, res) => {
     const { name, amount, unit } = req.body;
->>>>>>> main
 
     try {
         let cart = await ShoppingCart.findOne();
@@ -38,7 +35,7 @@ router.post("/add-item", async (req, res) => {
 });
 
 // Route to get all items in the shopping cart
-router.get("/items", async (req, res) => {
+router.get("/items", withAuth, async (req, res) => {
     try {
         // Find all items in the shopping cart
         const items = await ShoppingCartItem.findAll();
@@ -54,7 +51,7 @@ router.get("/items", async (req, res) => {
 
 
 // Route to delete an item from the shopping cart
-router.delete("/:name", async (req, res) => {
+router.delete("/:name", withAuth, async (req, res) => {
     const { name } = req.params;
 
     try {
