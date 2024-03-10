eadcc4ca21c738f7d427ac6ffae396f31ad180d6
ella gaul <145066673+mymashedpotatoes@users.noreply.github.com>
2024-03-10 00:47:38 -0600

    fixed put recipe route bug :P

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,12 @@ const shoppingCartRoutes = require('./shoppingCartRoutes');
 const recipeRoutes = require('./recipeRoutes');
 const userRoutes = require('./userRoutes');
 const mealRoutes = require('./mealRoutes');
-const recipeRoutes = require('./recipeRoutes');
 
-router.use('/recipe-routes', recipeRoutes);
+
+router.use('/recipe', recipeRoutes);
 router.use('/shopping-cart', shoppingCartRoutes);
 router.use('/users', userRoutes);
 router.use('/meals', mealRoutes);
-router.use('/recipe', mealRoutes);
+
 
 module.exports = router;
