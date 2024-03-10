db0ef0fe7710e67ea3fe16e8bd12780d9cab86a7
ella gaul <145066673+mymashedpotatoes@users.noreply.github.com>
2024-03-10 18:10:30 -0500

    fixed random routes to post to new recipe and add ingredients to shopping cart

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Recipe, Ingredient } = require("../../models");
-const withAuth = require('../utils/auth')
+const withAuth = require('../../utils/auth')
 
 
 
